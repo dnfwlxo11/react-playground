6a5cba00d44d0714cc90872d9006e2d0651df063
dnfwlxo11 <dnfwlxo11@naver.com>
2023-01-09 17:03:10 +0900

    finish: react redux example finish

--- react-redux-training/src/components/CounterContainer.js
+++ react-redux-training/src/components/CounterContainer.js
@@ -1,23 +1,22 @@
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
 function CounterContainer(props) {
-  const { number, increase, decrease } = props;
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
 
-  return <Counter 
-    number={number} 
-    onIncrease={increase} 
-    onDecrease={decrease}>
-  </Counter>
+  const onIncrease = useCallback(() => dispatch(increase()));
+  const onDecrease = useCallback(() => dispatch(decrease()));
+
+  return (
+    <Counter
+      number={number}
+      onIncrease={onIncrease}
+      onDecrease={onDecrease}
+    ></Counter>
+  );
 }
 
-export default connect(
-  ({counter}) => ({
-    number: counter.number
-  }),
-  {
-    increase,
-    decrease,
-  },
-)(CounterContainer);
+export default React.memo(CounterContainer);
--- react-redux-training/src/components/TodosContainer.js
+++ react-redux-training/src/components/TodosContainer.js
@@ -1,31 +1,30 @@
-import { connect } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
+import React, { useCallback } from 'react';
+import useActions from '../lib/useActions';
 
 function TodosContainer(props) {
-  const { input, todos, changeInput, insert, toggle, remove } = props;
-
-  return (
-    <Todos 
-      input={input} 
-      todos={todos} 
-      onChangeInput={changeInput} 
-      onInsert={insert} 
-      onToggle={toggle} 
-      onRemove={remove}>
-    </Todos>
-  )
-};
-
-export default connect(
-  ({todos}) => ({
+  const { input, todos } = useSelector(({ todos }) => ({
     input: todos.input,
     todos: todos.todos,
-  }),
-  {
-    changeInput,
-    insert,
-    toggle,
-    remove,
-  }
-)(TodosContainer);
+  }));
+
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    [],
+  );
+
+  return (
+    <Todos
+      input={input}
+      todos={todos}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
+    ></Todos>
+  );
+}
+
+export default React.memo(TodosContainer);
